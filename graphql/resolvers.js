bf7a50167e00a5d8d09857715a95da53ccb2b01c
BangHyeonJun <bhj2849@naver.com>
2019-05-26 14:38:10 +0900

    [ENH] 몽고 DB 연동 테스트 완료

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,21 @@
+import Movie from "./Movie";
+
 const resolvers = {
     Query: {
-        name: () => "방현준"
+        movie: (_, { title }) => {
+            return Movie.findOne({ title }); // 데이터베이스에서 특정 하나의 데이터 조회
+        }
+    },
+
+    Mutation: {
+        addMovie: async (_, { title, rating }) => {
+            let movie = new Movie({
+                title,
+                rating
+            });
+            console.log(movie.save());
+            return true; // 데이터베이스에 저장
+        }
     }
 };
 
--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 import { GraphQLServer } from "graphql-yoga";
+import mongoose from "mongoose";
 import resolvers from "./graphql/resolvers";
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(
+    "mongodb://localhost:27017/myBlog", // mLab URL 삽입 dbuser: 아이디, dbpassword: 비밀번호
+    { useNewUrlParser: true }
+);
+mongoose.connection.once("open", () => {
+    console.log("MongoDB Connected...");
+});
 
 const server = new GraphQLServer({
     typeDefs: "graphql/schema.graphql",
