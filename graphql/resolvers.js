8b8df89f412ce90c6c50d7f7234ee3b6607c68e6
david <david@acriil.com>
2019-06-28 18:41:27 +0900

    [ENH] 테스트 타입 추가

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,4 +1,5 @@
 import Post from "./Posts";
+import Test from "./Tests";
 import moment from "moment";
 import "moment-timezone";
 
@@ -6,6 +7,10 @@ const resolvers = {
     Query: {
         post: () => {
             return Post.find({}); // 데이터베이스에서 특정 하나의 데이터 조회
+        },
+
+        test: () => {
+            return Test.find({}); // TODO : 테스트를 진행하는 부분입니다, 빌드시 삭제해주세요
         }
     },
 
@@ -27,6 +32,18 @@ const resolvers = {
             });
 
             if (await post.save()) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+
+        insertTest: async (_, { text }) => {
+            let test = new Test({
+                text
+            });
+
+            if (await test.save()) {
                 return true;
             } else {
                 return false;
